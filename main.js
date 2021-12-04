06863ad7173376ece4bc3417848bfeb527cb64fe
tubeyOfficialYT <88552141+tubeyOfficialYT@users.noreply.github.com>
2021-12-04 20:33:27 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,34 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+  classifier = ml5.imageClassifier('mobileNet' ,modelLoaded);
 }
 
+function modelLoaded(){
+  console.log('model Loaded!');
+}
 
+function draw(){
+  image(vedio, 0, 0, 300, 300);
+  classifier.classify(video, gotResult);
+}
+var previous_result = '';
 
+function gotResult(error, results){
+  if(error){
+   console.error(error);
+   }else{
+     if((results[0].confidence > 0.5) && (previous_result != results[0].
+      label)){
+        console.log(results);
+        previous_result = results[0].label;
+        var synth = window.speechSynthesis;
+        speak_data = 'object detected is -'+results[0].label;
+        var utterthis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterthis);
+
+        document.getElementById("result_object_name").innerHTML = results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
+      }
+   }
+}
